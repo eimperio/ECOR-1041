aa79397d5604c41d32834e540053be890af3f6e8
eimperio <ethanimperio@hotmail.com>
2023-02-02 14:21:20 -0500

    work

---
+++ lab4 (1).py
@@ -0,0 +1,32 @@
+# ECOR 1041 Lab 4
+
+__author__ = ""
+__student_number__ = ""
+
+import math
+
+# =====================================================
+# Exercise 1
+
+# Type your function definition for Exercise 1 here.
+
+# =======================================================
+# Exercise 2
+
+# Type your function definition for Exercise 2 here.
+
+# =======================================================
+# Exercise 3
+
+# Type your function definition for Exercise 3 here.
+
+# =======================================================
+# Exercise 4
+
+# Type your function definition for Exercise 4 here.
+
+# =======================================================
+# Exercise 5
+
+# Type your function definitions for Exercise 5 here.
+
